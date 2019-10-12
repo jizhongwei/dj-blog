c0947a386e129c4ae4707bd740baf0a4248b23d4
dick <jzwxiaoniao@163.com>
2019-10-12 16:06:54 +0800

    完成博客blog的模型定义，存储模版资料

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length= 50)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length= 100)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add= True)
+    updated_time = models.DateTimeField(auto_now= True)
+    views = models.PositiveIntegerField(default= 0)
+    blog_img = models.ImageField(upload_to= 'blog/%Y-%m-%d')
+    tags = models.ManyToManyField(Tag)
+    category = models.ForeignKey(Category, on_delete= models.CASCADE)
+    author = models.ForeignKey(User, on_delete= models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('-created_time',)
